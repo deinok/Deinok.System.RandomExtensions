afb650095d1e94a42f3cfe13afb179e3814edcc5
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-22 23:23:32 +0200

    Extensions

---
+++ src/Deinok.System.RandomExtensions/RandomDateTimeExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace System {
+
+	public static class RandomDateTimeExtension{
+
+		public static DateTime NextDateTime(this Random random){
+			return new DateTime(random.Next());
+		}
+
+		public static DateTime NextDateTime(this Random random, DateTime min, DateTime max){
+			TimeSpan validRange = max - min;
+			return min.AddMilliseconds(random.NextDouble(0, validRange.TotalMilliseconds));
+		}
+	}
+
+
+}
--- src/Deinok.System.RandomExtensions/RandomExtension.cs
+++ src/Deinok.System.RandomExtensions/RandomExtension.cs
@@ -13,13 +13,7 @@
 			return random.Next() % 2 == 0;
 		}
 
-		public static DateTime NextDateTime(this Random random){
-			return new DateTime(random.Next());
-		}
-		public static DateTime NextDateTime(this Random random, DateTime min, DateTime max){
-			var validRange = max - min;
-			return min.AddMilliseconds(random.NextDouble(0, validRange.TotalMilliseconds));
-		}
+
 
 	}
 
