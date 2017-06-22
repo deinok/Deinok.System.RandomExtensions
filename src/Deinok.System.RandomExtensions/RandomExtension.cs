98a87104d226186003d59150ca438d92d31ac7f9
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-22 23:11:53 +0200

    Random

---
+++ src/Deinok.System.RandomExtensions/RandomExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace System {
+
+	public static class RandomExtension {
+
+		public static double NextDouble(this Random random, double maxValue){
+			return random.NextDouble(0, maxValue);
+		}
+		public static double NextDouble(this Random random, double minValue, double maxValue){
+			return random.NextDouble() * (maxValue - minValue) + maxValue;
+		}
+
+		public static bool NextBool(this Random random){
+			return random.Next() % 2 == 0;
+		}
+
+		public static DateTime NextDateTime(this Random random){
+			return new DateTime(random.Next());
+		}
+		public static DateTime NextDateTime(this Random random, DateTime min, DateTime max){
+			var validRange = max - min;
+			return min.AddMilliseconds(random.NextDouble(0, validRange.TotalMilliseconds));
+		}
+
+	}
+
+}
---
+++ src/Deinok.System.RandomExtensions/RandomFloatExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace System {
+
+	public static class RandomFloatExtension{
+
+		public static float NextFloat(this Random random){
+			return (float)random.NextDouble();
+		}
+
+		public static float NextFloat(this Random random, float maxValue){
+			return (float)random.NextDouble(maxValue);
+		}
+
+		public static float NextFloat(this Random random, float minValue, float maxValue){
+			return (float)random.NextDouble(minValue, maxValue);
+		}
+
+	}
+
+}
