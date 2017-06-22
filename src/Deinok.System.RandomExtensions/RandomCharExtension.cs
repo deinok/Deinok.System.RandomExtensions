9abcf6b5ea615c2c4928bbc606cb380408a9d164
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-23 00:45:32 +0200

    char

---
+++ src/Deinok.System.RandomExtensions/RandomCharExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace System {
+
+	public static class RandomCharExtension {
+
+		public static char NextChar(this Random random){
+			string chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
+			return chars[random.Next(chars.Length - 1)];
+		}
+
+	}
+
+}
