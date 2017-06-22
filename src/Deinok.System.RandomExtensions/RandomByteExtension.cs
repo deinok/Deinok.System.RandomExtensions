d97963519c44a71987102c697bf4c9f53d7c6c04
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-23 00:41:15 +0200

    Random Byte

---
+++ src/Deinok.System.RandomExtensions/RandomByteExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace System {
+
+	public static class RandomByteExtension {
+
+		public static byte NextByte(this Random random){
+			byte[] bytes = new byte[1];
+			random.NextBytes(bytes);
+			return bytes[0];
+		}
+
+	}
+
+}
