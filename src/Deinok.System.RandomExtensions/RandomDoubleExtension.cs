3d1b368088ddf97fd85f94afb445cbd49dd4d8d7
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-23 00:36:33 +0200

    RandomEnum

--- src/Deinok.System.RandomExtensions/RandomDoubleExtension.cs
+++ src/Deinok.System.RandomExtensions/RandomDoubleExtension.cs
@@ -1,16 +1,12 @@
 ﻿namespace System {
 
-	public static class RandomExtension {
+	public static class RandomDoubleExtension {
 
 		public static double NextDouble(this Random random, double maxValue){
 			return random.NextDouble(0, maxValue);
 		}
 		public static double NextDouble(this Random random, double minValue, double maxValue){
 			return random.NextDouble() * (maxValue - minValue) + maxValue;
-		}
-
-		public static bool NextBool(this Random random){
-			return random.Next() % 2 == 0;
 		}
 
 	}
---
+++ src/Deinok.System.RandomExtensions/RandomEnumExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace System {
+
+	public static class RandomEnumExtension {
+
+		public static E NextEnum<E>(this Random random){
+			Array values = Enum.GetValues(typeof(E));
+			return (E) values.GetValue(random.Next(values.Length));
+		}
+
+	}
+
+}
