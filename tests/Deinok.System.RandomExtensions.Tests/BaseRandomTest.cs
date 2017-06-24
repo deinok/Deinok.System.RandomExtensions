4ad3a1e78983037ba4b9eaab945732d66ce8c9d8
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-24 19:40:02 +0200

    Random Bool Test

---
+++ tests/Deinok.System.RandomExtensions.Tests/BaseRandomTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Deinok.System.RandomExtensions.Tests {
+
+	public abstract class BaseRandomTest{
+
+		protected Random Random { get; } = new Random();
+
+		public BaseRandomTest() { }
+
+    }
+
+}
---
+++ tests/Deinok.System.RandomExtensions.Tests/RandomBoolExtensionTest.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Deinok.System.RandomExtensions.Tests {
+
+	public class RandomBoolExtensionTest:BaseRandomTest{
+
+        [Fact]
+        public void Test1() {
+			this.Random.NextBool();
+        }
+
+		[Fact]
+		public void Test2(){
+			int numberElements = 1000000;
+			int maxPercentage = 60;
+			int maxTrues = (numberElements * maxPercentage) / 100;
+			int minPercentage = 40;
+			int minTrues = (numberElements * minPercentage) / 100;
+			var numberTrues =this.GenerateRandomBools().Take(numberElements).Count(boolVal=>boolVal);
+			Assert.True(numberTrues <= maxTrues && numberTrues >= minTrues);
+		}
+
+		private IEnumerable<bool> GenerateRandomBools(){
+			while (true) {
+				yield return this.Random.NextBool();
+			}
+		}
+
+    }
+
+}
