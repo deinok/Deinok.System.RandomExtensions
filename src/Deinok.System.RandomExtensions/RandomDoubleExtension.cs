e3758afe76ac3cc00e6449f1d795f1efad1c8b12
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-24 21:48:02 +0200

    double

--- src/Deinok.System.RandomExtensions/RandomDoubleExtension.cs
+++ src/Deinok.System.RandomExtensions/RandomDoubleExtension.cs
@@ -1,10 +1,25 @@
 ﻿namespace System {
 
+	/// <summary>
+	/// An extension to Random to generate doubles
+	/// </summary>
 	public static class RandomDoubleExtension {
 
-		public static double NextDouble(this Random random, double maxValue){
-			return random.NextDouble(0, maxValue);
-		}
+		/// <summary>
+		/// Get a random double
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="maxValue">The maximum value</param>
+		/// <returns>A random double</returns>
+		public static double NextDouble(this Random random, double maxValue) => random.NextDouble(0, maxValue);
+
+		/// <summary>
+		/// Get a random double
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="minValue">The minimum value</param>
+		/// <param name="maxValue">The maximum value</param>
+		/// <returns></returns>
 		public static double NextDouble(this Random random, double minValue, double maxValue){
 			return random.NextDouble() * (maxValue - minValue) + maxValue;
 		}
--- src/Deinok.System.RandomExtensions/RandomFloatExtension.cs
+++ src/Deinok.System.RandomExtensions/RandomFloatExtension.cs
@@ -1,11 +1,25 @@
 ﻿namespace System {
 
+	/// <summary>
+	/// A extension to random to generate floats
+	/// </summary>
 	public static class RandomFloatExtension{
 
+		/// <summary>
+		/// Get a random float
+		/// </summary>
+		/// <param name="random"></param>
+		/// <returns>A random float</returns>
 		public static float NextFloat(this Random random){
 			return (float)random.NextDouble();
 		}
 
+		/// <summary>
+		/// Get a random float
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="maxValue">The maximum value</param>
+		/// <returns>A random float</returns>
 		public static float NextFloat(this Random random, float maxValue){
 			return (float)random.NextDouble(maxValue);
 		}
