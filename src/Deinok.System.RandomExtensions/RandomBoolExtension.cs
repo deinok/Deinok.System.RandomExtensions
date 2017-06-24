ae0f694a2592164388b1c98f06844be19102537c
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-24 21:04:03 +0200

    Random byte

--- src/Deinok.System.RandomExtensions/RandomBoolExtension.cs
+++ src/Deinok.System.RandomExtensions/RandomBoolExtension.cs
@@ -8,7 +8,7 @@
 		/// <summary>
 		/// Get a random bool
 		/// </summary>
-		/// <param name="random">A Random</param>
+		/// <param name="random"></param>
 		/// <returns>A random bool</returns>
 		public static bool NextBool(this Random random){
 			return random.Next() % 2 == 0;
--- src/Deinok.System.RandomExtensions/RandomByteExtension.cs
+++ src/Deinok.System.RandomExtensions/RandomByteExtension.cs
@@ -2,12 +2,23 @@
 
 	public static class RandomByteExtension {
 
+		/// <summary>
+		/// Get a random byte
+		/// </summary>
+		/// <param name="random"></param>
+		/// <returns>A random byte</returns>
 		public static byte NextByte(this Random random){
 			byte[] bytes = new byte[1];
 			random.NextBytes(bytes);
 			return bytes[0];
 		}
 
+		/// <summary>
+		/// Get a random byte array
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="length">The length of the array</param>
+		/// <returns>A random byte[]</returns>
 		public static byte[] NextBytes(this Random random,int length){
 			byte[] bytes = new byte[length];
 			random.NextBytes(bytes);
