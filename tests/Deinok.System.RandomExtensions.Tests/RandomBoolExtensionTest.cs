7c9f9d4b7bb8e765dc2016d3b9713de1e1370e10
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-24 22:36:02 +0200

    Treat Warnings

--- tests/Deinok.System.RandomExtensions.Tests/RandomBoolExtensionTest.cs
+++ tests/Deinok.System.RandomExtensions.Tests/RandomBoolExtensionTest.cs
@@ -5,15 +5,24 @@ using Xunit;
 
 namespace Deinok.System.RandomExtensions.Tests {
 
+	/// <summary>
+	/// Tests of Bool
+	/// </summary>
 	public class RandomBoolExtensionTest:BaseRandomTest{
 
+		/// <summary>
+		/// Test NextBool
+		/// </summary>
         [Fact]
         public void NextBoolTest() {
 			Assert.IsType<bool>(this.Random.NextBool());
         }
 
+		/// <summary>
+		/// Test that random bool is between 40-60%
+		/// </summary>
 		[Fact]
-		public void Test2(){
+		public void ProbabilityTest(){
 			int numberElements = 1000000;
 			int maxPercentage = 60;
 			int maxTrues = (numberElements * maxPercentage) / 100;
--- tests/Deinok.System.RandomExtensions.Tests/RandomByteExtensionTest.cs
+++ tests/Deinok.System.RandomExtensions.Tests/RandomByteExtensionTest.cs
@@ -3,8 +3,14 @@ using Xunit;
 
 namespace Deinok.System.RandomExtensions.Tests {
 
+	/// <summary>
+	/// Test of bytes
+	/// </summary>
 	public class RandomByteExtensionTest:BaseRandomTest{
 
+		/// <summary>
+		/// Test NextByte
+		/// </summary>
 		[Fact]
 		public void NextByteTest(){
 			Assert.IsType<byte>(this.Random.NextByte());
