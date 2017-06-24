45ecf379d36d41fdb03ce0bf8657b449e4a59d86
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-24 22:33:39 +0200

    BaseTest

--- tests/Deinok.System.RandomExtensions.Tests/BaseRandomTest.cs
+++ tests/Deinok.System.RandomExtensions.Tests/BaseRandomTest.cs
@@ -2,11 +2,15 @@
 
 namespace Deinok.System.RandomExtensions.Tests {
 
+	/// <summary>
+	/// The base class to tests random
+	/// </summary>
 	public abstract class BaseRandomTest{
 
+		/// <summary>
+		/// A Random instance ready
+		/// </summary>
 		protected Random Random { get; } = new Random();
-
-		public BaseRandomTest() { }
 
     }
 
