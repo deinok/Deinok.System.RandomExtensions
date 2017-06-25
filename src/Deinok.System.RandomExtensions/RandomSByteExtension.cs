276b48e9a60640b4dfab8879c82558b587da5832
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-25 20:32:04 +0200

    SByte (#8)

---
+++ src/Deinok.System.RandomExtensions/RandomSByteExtension.cs
@@ -0,0 +1,34 @@
+﻿namespace System {
+
+	/// <summary>
+	/// An extension to Random to generate SBytes
+	/// </summary>
+	public static class RandomSByteExtension{
+
+		/// <summary>
+		/// Get a random SByte
+		/// </summary>
+		/// <param name="random"></param>
+		/// <returns>A random SByte</returns>
+		public static SByte NextSByte(this Random random) => random.NextSByte(SByte.MaxValue);
+
+		/// <summary>
+		/// Get a random SByte
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="maxValue">The maximum value</param>
+		/// <returns>A random SByte</returns>
+		public static SByte NextSByte(this Random random, SByte maxValue) => random.NextSByte(0, maxValue);
+
+		/// <summary>
+		/// Get a random SByte
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="minValue">The minimum value</param>
+		/// <param name="maxValue">The maximum value</param>
+		/// <returns>A random SByte</returns>
+		public static SByte NextSByte(this Random random, SByte minValue, SByte maxValue) => (SByte)random.NextInt16(minValue, maxValue);
+
+	}
+
+}
