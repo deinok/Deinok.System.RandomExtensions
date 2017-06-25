fab6d9c77208d198cc3498937e3f67ef6b7f9387
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-06-25 04:39:29 +0200

    string (#6)

---
+++ src/Deinok.System.RandomExtensions/RandomStringExtension.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace System {
+
+	/// <summary>
+	/// An extension to random to generate strings
+	/// </summary>
+	public static class RandomStringExtension {
+
+		/// <summary>
+		/// Get a random string
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="length">The length of the string</param>
+		/// <returns>A random string</returns>
+		public static string NextString(this Random random,int length){
+			return new string(new char[length]
+				.Select(selector => random.NextChar())
+				.ToArray()
+			);
+		}
+
+		/// <summary>
+		/// Get a random string
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="length">The length of the string</param>
+		/// <param name="chars">Available chars</param>
+		/// <returns>A random string</returns>
+		public static string NextString(this Random random,int length,IEnumerable<char> chars){
+			return new string(new char[length]
+				.Select(selector => random.NextChar(chars))
+				.ToArray()
+			);
+		}
+
+		/// <summary>
+		/// Get a random ASCII string
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="length">The length of the string</param>
+		/// <returns>A random ASCII string</returns>
+		public static string NextStringAscii(this Random random,int length){
+			return new string(new char[length]
+				.Select(selector => random.NextCharAscii())
+				.ToArray()
+			);
+		}
+
+		/// <summary>
+		/// Get a random printable ASCII string
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="length">The length of the string</param>
+		/// <returns>A random printable ASCII string</returns>
+		public static string NextStringPrintableAscii(this Random random,int length){
+			return new string(new char[length]
+				.Select(selector => random.NextCharPrintableAscii())
+				.ToArray()
+			);
+		}
+
+	}
+
+}
