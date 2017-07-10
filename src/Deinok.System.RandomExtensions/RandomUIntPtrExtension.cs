5c34a307e42206df7a619b2c2a309046b66a348b
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-07-10 05:16:27 +0200

    UIntPtr (#12)

---
+++ src/Deinok.System.RandomExtensions/RandomUIntPtrExtension.cs
@@ -0,0 +1,36 @@
+﻿namespace System {
+
+	/// <summary>
+	/// An extension to Random to generate UIntPtrs
+	/// </summary>
+	public static class RandomUIntPtrExtension{
+
+		/// <summary>
+		/// Get a random UIntPtr
+		/// </summary>
+		/// <param name="random"></param>
+		/// <returns>A random UIntPtr</returns>
+		public static UIntPtr NextUIntPtr(this Random random) => random.NextUIntPtr(new UIntPtr(UInt64.MaxValue));
+
+		/// <summary>
+		/// Get a random UIntPtr
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="maxValue">The maximum value</param>
+		/// <returns>A random UIntPtr</returns>
+		public static UIntPtr NextUIntPtr(this Random random, UIntPtr maxValue) => random.NextUIntPtr(new UIntPtr(0), maxValue);
+
+		/// <summary>
+		/// Get a random UIntPtr
+		/// </summary>
+		/// <param name="random"></param>
+		/// <param name="minValue">The minimum value</param>
+		/// <param name="maxValue">The maximum value</param>
+		/// <returns>A random UIntPtr</returns>
+		public static UIntPtr NextUIntPtr(this Random random, UIntPtr minValue, UIntPtr maxValue){
+			return new UIntPtr(random.NextUInt64(minValue.ToUInt64(),maxValue.ToUInt64()));
+		}
+
+	}
+
+}
